01db93efbae3bb83108dd26d4367e6c4305dafb0
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-17 22:38:06 -0700

    Adding a facts array

--- app/src/main/java/com/virajkulkarni/cmpe277/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/virajkulkarni/cmpe277/funfacts/FunFactsActivity.java
@@ -26,22 +26,26 @@ public class FunFactsActivity extends AppCompatActivity {
 		View.OnClickListener listener = new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				String[] facts = {
+						"Ants stretch when they wake up in the morning.",
+						"Ostriches can run faster than horses.",
+						"Olympic gold medals are actually made mostly of silver.",
+						"You are born with 300 bones; by the time you are an adult you will have 206.",
+						"It takes about 8 minutes for light from the Sun to reach Earth.",
+						"Some bamboo plants can grow almost a meter in just one day.",
+						"The state of Florida is bigger than England.",
+						"Some penguins can leap 2-3 meters out of the water.",
+						"On average, it takes 66 days to form a new habit.",
+						"Mammoths still walked the earth when the Great Pyramid was being built."
+				};
+
 				// When button is clicked, update fact label with a new fact!
 				String fact = "";
 
 				Random randomGenerator = new Random();
-				int randomNumber = randomGenerator.nextInt(3);
+				int randomNumber = randomGenerator.nextInt(facts.length);
 
-				//convert randomNumber to text fact
-				if (randomNumber == 0) {
-					fact = "Ants stretch when they wake up in the morning.";
-				} else if (randomNumber == 1) {
-					fact = "Ostriches can run faster than horses.";
-				} else if (randomNumber == 2) {
-					fact  = "Olympic gold medals are actually made mostly out of silver.";
-				} else {
-					fact = "Looks like there was an error!";
-				}
+				fact = facts[randomNumber];
 
 				// Instead of hard-coding, update the label with dynamic fact
 
