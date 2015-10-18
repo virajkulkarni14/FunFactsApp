299cb2012da7c3185e163d50275ecc91306f8dfb
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-17 22:59:50 -0700

    Refactoring to separate Facts from FactsActivity

--- app/src/main/java/com/virajkulkarni/cmpe277/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/virajkulkarni/cmpe277/funfacts/FunFactsActivity.java
@@ -15,6 +15,8 @@ import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
 
+	private FactBook mFactBook = new FactBook(); // m = Member var
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,54 +28,12 @@ public class FunFactsActivity extends AppCompatActivity {
 		View.OnClickListener listener = new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String[] facts = {
-						"Ants stretch when they wake up in the morning.",
-						"Ostriches can run faster than horses.",
-						"Olympic gold medals are actually made mostly of silver.",
-						"You are born with 300 bones; by the time you are an adult you will have 206.",
-						"It takes about 8 minutes for light from the Sun to reach Earth.",
-						"Some bamboo plants can grow almost a meter in just one day.",
-						"The state of Florida is bigger than England.",
-						"Some penguins can leap 2-3 meters out of the water.",
-						"On average, it takes 66 days to form a new habit.",
-						"Mammoths still walked the earth when the Great Pyramid was being built."
-				};
-
-				// When button is clicked, update fact label with a new fact!
-				String fact = "";
-
-				Random randomGenerator = new Random();
-				int randomNumber = randomGenerator.nextInt(facts.length);
-
-				fact = facts[randomNumber];
-
+				String fact = mFactBook.getFact();
 				// Instead of hard-coding, update the label with dynamic fact
 
 				factLabel.setText(fact);
 			}
 		};
 		showFactButton.setOnClickListener(listener);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 }
